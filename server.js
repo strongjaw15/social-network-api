8c881d73e3a9ec29c48e0302bd7b043460a970de
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-22 22:27:11 -0600

    Set up the server and connection files.

---
+++ server.js
@@ -0,0 +1,17 @@
+// const path = require('path')
+const express = require('express')
+const db = require('./config/connection');
+const routes = require('./controllers')
+
+const PORT = process.env.PORT || 3001
+const app = express()
+
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(routes)
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Now listening at http://localhost:${PORT}`);
+  });
+});
