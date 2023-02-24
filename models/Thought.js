f908ad1a4782211c3b44134e91d05a3332a4b7e8
strongjaw15 <119770691+strongjaw15@users.noreply.github.com>
2023-02-24 10:10:09 -0600

    Worked on reactions subdocument.

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,8 @@
 const {Schema, model} = require('mongoose')
 const moment = require('moment')
 
+const reactionSchema = new Schema({})
+
 const thoughtSchema = new Schema({
   thoughtText: {
     type: String,
@@ -17,7 +19,7 @@ const thoughtSchema = new Schema({
     type: String,
     required: true
   },
-  reactions: []
+  reactions: [Reaction]
 },
 {
   toJSON: {
